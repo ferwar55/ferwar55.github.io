e6abacc560ab4bf780a003751aa7f98669a46151
ferwar55 <124490122+ferwar55@users.noreply.github.com>
2023-02-08 21:09:12 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click', function () {
+    alert('Te quiero mucho, mi querida Hu 💖')
+    alert('Me haces la persona más feliz')
+    location.href = "https://www.youtube.com/watch?v=vOMoivMmSP8&list=RDvOMoivMmSP8&start_radio=1"
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('click', function () {
+    alert("No me rendire")
+})
