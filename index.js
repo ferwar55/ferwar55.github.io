e61738019334e87ed215b383220267eb3f09740e
ferwar55 <124490122+ferwar55@users.noreply.github.com>
2026-09-22 11:35:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const yesBtn = document.querySelector('#yesBtn');
 yesBtn.addEventListener('click', function () {
     alert('Te quiero mucho, mi querida Hu 💖')
     alert('Me haces la persona más feliz')
-    location.href = "https://www.youtube.com/watch?v=vOMoivMmSP8&list=RDvOMoivMmSP8&start_radio=1"
-});
+    location.href = "https://www.youtube.com/watch?v=uZO-P4ukFxE&list=RDuZO-P4ukFxE&start_radio=1
 
 const noBtn = document.querySelector('#noBtn');
 
