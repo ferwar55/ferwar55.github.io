40b0c5e97e170f6f49dc004f8b9d9ecf7f9b7344
ferwar55 <124490122+ferwar55@users.noreply.github.com>
2026-09-26 00:19:25 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ yesBtn.addEventListener('click', function () {
     alert('Te quiero mucho, mi querida Hu 💖')
     alert('Me haces la persona más feliz')
     location.href = "https://www.youtube.com/watch?v=uZO-P4ukFxE&list=RDuZO-P4ukFxE&start_radio=1"
-
+});
 const noBtn = document.querySelector('#noBtn');
 
 noBtn.addEventListener('click', function () {
